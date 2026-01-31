760c9c8228b8fbf5a33352fdd9e3b2afea0db42b
SokKimThanh <thanhsk1991@gmail.com>
2026-01-31 19:32:36 +0700

    Add CategoryController and CategoryRepository for managing categories

---
+++ src/main/java/com/smartfashion/erp/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.smartfashion.erp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartfashion.erp.entity.Category;
+import com.smartfashion.erp.repogistory.CategoryRepository;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository repository;
+
+    @GetMapping
+    public List<Category> getAll() {
+        return repository.findAll();
+    }
+}
